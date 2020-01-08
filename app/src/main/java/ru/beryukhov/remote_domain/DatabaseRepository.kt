695bb38815c3559c87d908c743c60f6af98542b5
Andrey Beryukhov <beryukhov-andrey@yandex.ru>
2020-01-08 16:57:13 +0300

    SQLDelight listen for table changes

--- app/src/main/java/ru/beryukhov/remote_domain/DatabaseRepository.kt
+++ app/src/main/java/ru/beryukhov/remote_domain/DatabaseRepository.kt
@@ -1,28 +1,64 @@
 package ru.beryukhov.remote_domain
 
 import android.content.Context
+import android.util.Log
+import com.squareup.sqldelight.Query
 import com.squareup.sqldelight.android.AndroidSqliteDriver
 import com.squareup.sqldelight.db.SqlDriver
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 
 /**
  * Created by Andrey Beryukhov
  */
+@UseExperimental(
+    InternalCoroutinesApi::class,
+    ExperimentalCoroutinesApi::class,
+    ObsoleteCoroutinesApi::class
+)
 fun testDb(context: Context, log: suspend (String) -> Unit) {
     val driver: SqlDriver = AndroidSqliteDriver(Database.Schema, context, "test.db")
     val database = Database(driver)
     val userQueries: UserQueries = database.userQueries
+    Log.d("DR_", "1")
+    val dbContext = newSingleThreadContext("DB")
+    CoroutineScope(dbContext).launch {
 
-    GlobalScope.launch {
+        Log.d("DR_", "2")
         userQueries.createUserTable()
+        Log.d("DR_", "3")
+        userQueries.selectAll().addListener(object : Query.Listener {
+            override fun queryResultsChanged() {
+                Log.d("DR_", "onEach")
+                val result = userQueries.selectAll().executeAsList().toString()
+                CoroutineScope(dbContext).launch {
+                    log(result)
+                }
+            }
+        })
 
+        //userQueries.selectAll().asFlow().flowOn(Dispatchers.IO).collect { value -> log(value.executeAsList().toString()) }
+        /*userQueries.selectAll().asFlow().onEach { value ->
+            run {
+
+                Log.d("DR_", "onEach")
+                //log(value.executeAsList().toString())
+                val result = value.executeAsList().toString()
+                CoroutineScope(dbContext).launch {
+                    log(result)
+                }
+            }
+        }*/
+
+        Log.d("DR_", "emit 1")
         userQueries.insertUser(User.Impl("user_id0", "user_name"))
+        Log.d("DR_", "emit 2")
         userQueries.insertUser(User.Impl("user_id1", "user_name"))
+        Log.d("DR_", "emit 3")
         userQueries.insertUser(User.Impl("user_id2", "user_name"))
+        Log.d("DR_", "emit 4")
         userQueries.insertUser(User.Impl("user_id3", "user_name"))
 
-        log(userQueries.selectAll().executeAsList().toString())
+        //log(userQueries.selectAll().executeAsList().toString())
 
         userQueries.deleteUserTable()
     }

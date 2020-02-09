71d2e97e5837fbcbca758fdfee1921685ae4e8e9
Andrey Beryukhov <beryukhov-andrey@yandex.ru>
2020-02-09 16:56:30 +0300

    Try to setup ktor websockets in android

--- app/src/main/java/ru/beryukhov/remote_domain/NetworkRepository.kt
+++ app/src/main/java/ru/beryukhov/remote_domain/NetworkRepository.kt
@@ -5,11 +5,18 @@ import io.ktor.client.engine.okhttp.OkHttp
 import io.ktor.client.features.ResponseException
 import io.ktor.client.features.json.GsonSerializer
 import io.ktor.client.features.json.JsonFeature
+import io.ktor.client.features.websocket.WebSockets
+import io.ktor.client.features.websocket.ws
 import io.ktor.client.request.delete
 import io.ktor.client.request.get
 import io.ktor.client.request.post
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
+import io.ktor.http.HttpMethod
+import io.ktor.http.cio.websocket.Frame
+import io.ktor.http.cio.websocket.readBytes
+import io.ktor.http.cio.websocket.readText
+import io.ktor.http.cio.websocket.send
+import io.ktor.util.KtorExperimentalAPI
+import kotlinx.coroutines.*
 import okhttp3.logging.HttpLoggingInterceptor
 import ru.beryukhov.common.model.CompletableResult
 import ru.beryukhov.common.model.Post
@@ -18,6 +25,11 @@ import ru.beryukhov.common.model.User
 import ru.beryukhov.remote_domain.NetworkRepository.testError
 import ru.beryukhov.remote_domain.NetworkRepository.testPosts
 import ru.beryukhov.remote_domain.NetworkRepository.testUsers
+import java.time.Duration
+
+private const val BASE_URL = "evening-forest-47787.herokuapp.com"
+private const val SOCKER_URL = "ws://$BASE_URL"
+private const val SERVER_URL = "http://$BASE_URL"
 
 /**
  * Created by Andrey Beryukhov
@@ -47,15 +59,50 @@ fun testHttp(log: suspend (String) -> Unit) {
     }
 }
 
+@KtorExperimentalAPI
 fun testSocket(log: suspend (String) -> Unit){
+    //main thread
+    CoroutineScope(Dispatchers.IO).launch {
+        log("Test Socket: ")
+        try {
+            val client = HttpClient(OkHttp.create()) {
+                install(WebSockets) {
 
+                }
+            }
+
+            client.ws(
+                method = HttpMethod.Get,
+                host = SOCKER_URL,
+                //port = 8080,
+                path = "/ws"
+            ) {
+                send(Frame.Text("Hello World"))
+                timeoutMillis = 60_000 * 15
+                pingIntervalMillis = 1_000
+                while (true) {
+                    // Receive frame.
+                    val frame = incoming.receive()
+                    log("WS received")
+                    when (frame) {
+                        is Frame.Text -> log("WS received Text: ${frame.readText()}")
+                        is Frame.Binary -> log("WS received Binary: ${frame.readBytes()}")
+                    }
+                }
+            }
+
+            client.close()
+        }
+        catch (e: Throwable){
+            log("WS error: ${e.message}")
+        }
+    }
 }
 
 
 
 object NetworkRepository {
     //private val SERVER_URL = "http://10.0.2.2:8080"
-    private const val SERVER_URL = "http://evening-forest-47787.herokuapp.com"
     private const val HEADER_CONTENT_TYPE = "Content-Type"
     private const val HEADER_JSON = "application/json"
 

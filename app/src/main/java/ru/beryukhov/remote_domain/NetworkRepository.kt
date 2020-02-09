16fec63caf5942d68326aa916bcc26259fb86ea4
Andrey Beryukhov <beryukhov-andrey@yandex.ru>
2020-02-09 17:18:43 +0300

    Okhttp sockets

--- app/src/main/java/ru/beryukhov/remote_domain/NetworkRepository.kt
+++ app/src/main/java/ru/beryukhov/remote_domain/NetworkRepository.kt
@@ -14,10 +14,12 @@ import io.ktor.http.HttpMethod
 import io.ktor.http.cio.websocket.Frame
 import io.ktor.http.cio.websocket.readBytes
 import io.ktor.http.cio.websocket.readText
-import io.ktor.http.cio.websocket.send
 import io.ktor.util.KtorExperimentalAPI
 import kotlinx.coroutines.*
+import okhttp3.*
 import okhttp3.logging.HttpLoggingInterceptor
+import okio.ByteString
+import okio.ByteString.Companion.decodeHex
 import ru.beryukhov.common.model.CompletableResult
 import ru.beryukhov.common.model.Post
 import ru.beryukhov.common.model.Result
@@ -25,10 +27,10 @@ import ru.beryukhov.common.model.User
 import ru.beryukhov.remote_domain.NetworkRepository.testError
 import ru.beryukhov.remote_domain.NetworkRepository.testPosts
 import ru.beryukhov.remote_domain.NetworkRepository.testUsers
-import java.time.Duration
+
 
 private const val BASE_URL = "evening-forest-47787.herokuapp.com"
-private const val SOCKER_URL = "ws://$BASE_URL"
+private const val SOCKET_URL = "ws://$BASE_URL"
 private const val SERVER_URL = "http://$BASE_URL"
 
 /**
@@ -59,8 +61,58 @@ fun testHttp(log: suspend (String) -> Unit) {
     }
 }
 
-@KtorExperimentalAPI
 fun testSocket(log: suspend (String) -> Unit){
+    val client = OkHttpClient()
+    val request: Request = Request.Builder().url("$SOCKET_URL/ws").build()
+    val listener = EchoWebSocketListener(log)
+    val ws: WebSocket = client.newWebSocket(request, listener)
+    client.dispatcher.executorService.shutdown()
+}
+
+private class EchoWebSocketListener(private val log: suspend (String) -> Unit) : WebSocketListener() {
+    fun output(s: String){
+        GlobalScope.launch {
+            withContext(Dispatchers.Main) {
+                log(s)
+            }
+        }
+    }
+
+    override fun onOpen(webSocket: WebSocket, response: Response) {
+        /*webSocket.send("Hello, it's SSaurel !")
+        webSocket.send("What's up ?")
+        webSocket.send("deadbeef".decodeHex())
+        webSocket.close(NORMAL_CLOSURE_STATUS, "Goodbye !")*/
+    }
+
+    override fun onMessage(webSocket: WebSocket, text: String) {
+        output("Receiving : $text")
+    }
+
+    override fun onMessage(webSocket: WebSocket, bytes: ByteString) {
+        output("Receiving bytes : " + bytes.hex())
+    }
+
+    override fun onClosing(
+        webSocket: WebSocket,
+        code: Int,
+        reason: String
+    ) {
+        webSocket.close(NORMAL_CLOSURE_STATUS, null)
+        output("Closing : $code / $reason")
+    }
+
+    override fun onFailure(webSocket: WebSocket, t: Throwable, response: Response?) {
+        output("Error : " + t.message)
+    }
+
+    companion object {
+        private const val NORMAL_CLOSURE_STATUS = 1000
+    }
+}
+
+@KtorExperimentalAPI
+fun testSocket1(log: suspend (String) -> Unit){
     //main thread
     CoroutineScope(Dispatchers.IO).launch {
         log("Test Socket: ")
@@ -73,7 +125,7 @@ fun testSocket(log: suspend (String) -> Unit){
 
             client.ws(
                 method = HttpMethod.Get,
-                host = SOCKER_URL,
+                host = SOCKET_URL,
                 //port = 8080,
                 path = "/ws"
             ) {

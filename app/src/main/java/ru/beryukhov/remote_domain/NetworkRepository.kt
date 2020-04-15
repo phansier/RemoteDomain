0018974ceef27eb51a563ee5e4acc3cd3d7c2fa2
Andrey Beryukhov <andrey.beryukhov@kaspersky.com>
2020-04-15 20:33:27 +0300

    Error branch because of strong typization

--- app/src/main/java/ru/beryukhov/remote_domain/NetworkRepository.kt
+++ app/src/main/java/ru/beryukhov/remote_domain/NetworkRepository.kt
@@ -1,20 +1,13 @@
 package ru.beryukhov.remote_domain
 
-import io.ktor.client.HttpClient
-//import io.ktor.client.engine.okhttp.OkHttp
-import io.ktor.client.features.json.GsonSerializer
-import io.ktor.client.features.json.JsonFeature
-import io.ktor.client.request.get
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
-//import okhttp3.logging.HttpLoggingInterceptor
+import ru.beryukhov.client_lib.http.ClientApi
+import ru.beryukhov.client_lib.http.HttpClientRepositoryImpl
+import ru.beryukhov.common.model.Entity
 import ru.beryukhov.common.model.Post
-import ru.beryukhov.common.model.Result
 import ru.beryukhov.common.model.User
-import ru.beryukhov.remote_domain.NetworkRepository.testError
-import ru.beryukhov.remote_domain.NetworkRepository.testPosts
-import ru.beryukhov.remote_domain.NetworkRepository.testUsers
-import ru.beryukhov.client_lib.http.BaseHttpClient
+
 import ru.beryukhov.remote_domain.http.ClientPostApi
 import ru.beryukhov.remote_domain.http.ClientUserApi
 
@@ -27,69 +20,57 @@ const val SERVER_URL = "http://$BASE_URL"
  * Created by Andrey Beryukhov
  */
 fun testHttp(log: suspend (String) -> Unit) {
-    //todo return
-    /*GlobalScope.launch {
-        val interceptor = HttpLoggingInterceptor()
-        interceptor.level =
-            if (BuildConfig.DEBUG) HttpLoggingInterceptor.Level.BODY else HttpLoggingInterceptor.Level.NONE
-        val client = HttpClient(OkHttp) {
-            install(JsonFeature) {
-                serializer = GsonSerializer {
-                    serializeNulls()
-                    disableHtmlEscaping()
-                }
-            }
-            engine {
-                addInterceptor(interceptor)
-            }
+    GlobalScope.launch {
+        val httpClientRepository = HttpClientRepositoryImpl(SERVER_URL, BuildConfig.DEBUG, log)
+        httpClientRepository.addClientApi(
+            User::class,
+            ClientUserApi(httpClientRepository.httpClient, SERVER_URL, log) as ClientApi<in Entity>
+        )
+        httpClientRepository.addClientApi(
+            Post::class,
+            ClientPostApi(httpClientRepository.httpClient, SERVER_URL, log) as ClientApi<in Entity>
+        )
 
+        val clientUserApi = httpClientRepository.getClientApi(User::class)!!
+        val clientPostApi = httpClientRepository.getClientApi(Post::class)!!
+        /*clientPostApi.testPosts(log)
+        clientUserApi.testUsers(log)*/
+        with(clientPostApi){
+            get("post")
+            create(Post("0", "1", "New post"), "post")
+            get("post")
+            delete(Post("-1", "-1", "Test Post //Todo Remove"), "post")
+            get("post")
         }
-
-        client.testPosts(log)
-        client.testUsers(log)
-        client.testError(log)
-
-        client.close()
-    }*/
+        with(clientUserApi){
+            get("user")
+            create(User("0", "New user"),"user")
+            get("user")
+            delete(User("-1", "Test Testov //Todo Remove"),"user")
+            get("user")
+        }
+    }
 }
 
 
-object NetworkRepository : BaseHttpClient() {
+/*object NetworkRepository {
 
-    suspend fun HttpClient.testPosts(log: suspend (String) -> Unit) {
-        val clientPostApi =
-            ClientPostApi(this, SERVER_URL, log)
-
-        //todo return
-        /*with(clientPostApi) {
-            getPosts()
-            createPost("1", "New post")
-            getPosts()
-            deletePost(Post("-1", "-1", "Test Post //Todo Remove"))
-            getPosts()
-        }*/
+    suspend fun ClientApi<in Entity>.testPosts(log: suspend (String) -> Unit) {
+        get("post")
+        create(Post("0", "1", "New post"), "post")
+        get("post")
+        delete(Post("-1", "-1", "Test Post //Todo Remove"), "post")
+        get("post")
     }
 
-    suspend fun HttpClient.testUsers(log: suspend (String) -> Unit) {
-        val clientPostApi =
-            ClientUserApi(this, SERVER_URL, log)
+    suspend fun ClientApi<in Entity>.testUsers(log: suspend (String) -> Unit) {
 
-        //todo return
-        /*with(clientPostApi) {
-            getUsers()
-            createUser("New user")
-            getUsers()
-            deleteUser(User("-1", "Test Testov //Todo Remove"))
-            getUsers()
-        }*/
+        get("user")
+        create(User("0","New user"),"user")
+        get("user")
+        delete(User("-1", "Test Testov //Todo Remove"),"user")
+        get("user")
     }
 
-    suspend fun HttpClient.testError(log: suspend (String) -> Unit) {
-        //todo return
-        /*makeRequest("get<Result.Success<List<User>>>(\"$SERVER_URL/error\")", log) {
-            get<Result.Success<List<User>>>("$SERVER_URL/error")
-        }*/
-    }
-
-}
+}*/
 

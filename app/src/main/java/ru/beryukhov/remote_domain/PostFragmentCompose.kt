de2dabd48876b3dcbd79e2e2ac489179b75fb76a
Andrey Beryukhov <beryukhov-andrey@yandex.ru>
2020-10-22 23:32:51 +0300

    Improve UI

--- app/src/main/java/ru/beryukhov/remote_domain/PostFragmentCompose.kt
+++ app/src/main/java/ru/beryukhov/remote_domain/PostFragmentCompose.kt
@@ -5,7 +5,11 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.compose.foundation.Text
 import androidx.compose.foundation.layout.ConstraintLayout
+import androidx.compose.foundation.layout.Dimension
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
+import androidx.compose.material.ContainerAlpha
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.TextField
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
@@ -30,22 +34,23 @@ class PostFragmentCompose : Fragment() {
 
 @Composable
 fun PostPage(post_id: String = "0") {
-    ConstraintLayout() {
+    ConstraintLayout(modifier = Modifier.padding(24.dp)) {
         val (postIdRef, userTextFieldRef, textFiledRef, buttonRef, deleteButtonRef) = createRefs()
         Text(text = post_id, modifier = Modifier.constrainAs(postIdRef) {
-            top.linkTo(parent.top, margin = 24.dp)
-            start.linkTo(parent.start, margin = 24.dp)
-            end.linkTo(parent.end, margin = 24.dp)
+            top.linkTo(parent.top, margin = 0.dp)
+            start.linkTo(parent.start, margin = 0.dp)
         })
         var userName: String by remember { mutableStateOf("") }
         TextField(
             value = userName,
             onValueChange = { userName = it },
             label = { Text("UserName") },
+            backgroundColor = MaterialTheme.colors.background.copy(alpha = ContainerAlpha),
             modifier = Modifier.constrainAs(userTextFieldRef) {
                 top.linkTo(postIdRef.bottom, margin = 4.dp)
-                start.linkTo(parent.start, margin = 24.dp)
-                end.linkTo(parent.end, margin = 24.dp)
+                start.linkTo(parent.start, margin = 0.dp)
+                end.linkTo(parent.end, margin = 0.dp)
+                width = Dimension.fillToConstraints
             }
         )
         var message: String by remember { mutableStateOf("") }
@@ -53,16 +58,18 @@ fun PostPage(post_id: String = "0") {
             value = message,
             onValueChange = { message = it },
             label = { Text("Message") },
+            backgroundColor = MaterialTheme.colors.background.copy(alpha = ContainerAlpha),
             modifier = Modifier.constrainAs(textFiledRef) {
                 top.linkTo(userTextFieldRef.bottom, margin = 4.dp)
-                start.linkTo(parent.start, margin = 24.dp)
-                end.linkTo(parent.end, margin = 24.dp)
+                start.linkTo(parent.start, margin = 0.dp)
+                end.linkTo(parent.end, margin = 0.dp)
+                width = Dimension.fillToConstraints
             }
         )
         Button(onClick = {},
             modifier = Modifier.constrainAs(buttonRef) {
                 top.linkTo(textFiledRef.bottom, margin = 16.dp)
-                start.linkTo(parent.start, margin = 24.dp)
+                start.linkTo(parent.start, margin = 8.dp)
                 end.linkTo(deleteButtonRef.start, margin = 8.dp)
             }) {
             Text("Save")
@@ -73,7 +80,7 @@ fun PostPage(post_id: String = "0") {
             modifier = Modifier.constrainAs(deleteButtonRef) {
                 top.linkTo(textFiledRef.bottom, margin = 16.dp)
                 start.linkTo(buttonRef.end, margin = 8.dp)
-                end.linkTo(parent.end, margin = 24.dp)
+                end.linkTo(parent.end, margin = 8.dp)
             }) {
             Text("Delete")
         }

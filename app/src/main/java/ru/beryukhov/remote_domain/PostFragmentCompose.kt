77aad34bb23dc049cd80c47c4fd3540c162ddd0a
Andrey Beryukhov <beryukhov-andrey@yandex.ru>
2020-10-30 19:31:33 +0300

    Start to migrate logic

--- app/src/main/java/ru/beryukhov/remote_domain/PostFragmentCompose.kt
+++ app/src/main/java/ru/beryukhov/remote_domain/PostFragmentCompose.kt
@@ -2,6 +2,7 @@ package ru.beryukhov.remote_domain
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.compose.foundation.Text
 import androidx.compose.foundation.layout.ConstraintLayout
@@ -18,6 +19,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import androidx.ui.tooling.preview.Preview
 import ru.beryukhov.client_lib.RemoteDomainClient
@@ -34,22 +36,56 @@ class PostFragmentCompose : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ) = ComposeView(
-        requireContext(),
-        attrs = null,
-        defStyleAttr = 0
-    ).apply { setContent { PostPage() } }
+    ): View {
+        val post = args.post
+        val users = remoteDomainClient.getEntity().users()!!
+        //todo add users list to edittext for autocomplete
+
+        val postPageData = if (post != null) {
+            val user = users.find { it.id == post.userId }
+            val userString = if (BuildConfig.SHOW_ENTITY_ID)
+                user.toString()
+            else
+                user?.userName
+            val deleteButtonOnClickListener: () -> Unit = {
+                remoteDomainClient.pushChanges(
+                    post.deleteDiff
+                )
+                findNavController().popBackStack()
+            }
+            PostPageData(
+                postIdText = post.id,
+                userText = userString ?: "",
+                messageText = post.message,
+                deleteButtonOnClick = deleteButtonOnClickListener
+            )
+        } else PostPageData()
+
+        return ComposeView(
+            requireContext(),
+            attrs = null,
+            defStyleAttr = 0
+        ).apply { setContent { PostPage(postPageData) } }
+    }
 }
 
+data class PostPageData(
+    val postIdText: String = "",
+    val userText: String = "",
+    val messageText: String = "",
+    val deleteButtonOnClick: () -> Unit = {}
+)
+
 @Composable
-fun PostPage(post_id: String = "0") {
+fun PostPage(postPageData: PostPageData = PostPageData()) {
+
     ConstraintLayout(modifier = Modifier.padding(24.dp)) {
         val (postIdRef, userTextFieldRef, textFiledRef, buttonRef, deleteButtonRef) = createRefs()
-        Text(text = post_id, modifier = Modifier.constrainAs(postIdRef) {
+        Text(text = postPageData.postIdText, modifier = Modifier.constrainAs(postIdRef) {
             top.linkTo(parent.top, margin = 0.dp)
             start.linkTo(parent.start, margin = 0.dp)
         })
-        var userName: String by remember { mutableStateOf("") }
+        var userName: String by remember { mutableStateOf(postPageData.userText) }
         TextField(
             value = userName,
             onValueChange = { userName = it },
@@ -62,7 +98,7 @@ fun PostPage(post_id: String = "0") {
                 width = Dimension.fillToConstraints
             }
         )
-        var message: String by remember { mutableStateOf("") }
+        var message: String by remember { mutableStateOf(postPageData.messageText) }
         TextField(
             value = message,
             onValueChange = { message = it },
@@ -75,25 +111,31 @@ fun PostPage(post_id: String = "0") {
                 width = Dimension.fillToConstraints
             }
         )
-        Button(onClick = {},
-            modifier = Modifier.constrainAs(buttonRef) {
-                top.linkTo(textFiledRef.bottom, margin = 16.dp)
-                start.linkTo(parent.start, margin = 8.dp)
-                end.linkTo(deleteButtonRef.start, margin = 8.dp)
-            }) {
-            Text("Save")
+        var saveButtonVisible: Boolean by remember { mutableStateOf(false) }
+        if (saveButtonVisible) {
+            Button(onClick = {},
+                modifier = Modifier.constrainAs(buttonRef) {
+                    top.linkTo(textFiledRef.bottom, margin = 16.dp)
+                    start.linkTo(parent.start, margin = 8.dp)
+                    end.linkTo(deleteButtonRef.start, margin = 8.dp)
+                }) {
+                Text("Save")
+            }
         }
-        Button(onClick = {},
-            colors = defaultButtonColors(
-                backgroundColor = Color.Red,
-                contentColor = Color.White
-            ),
-            modifier = Modifier.constrainAs(deleteButtonRef) {
-                top.linkTo(textFiledRef.bottom, margin = 16.dp)
-                start.linkTo(buttonRef.end, margin = 8.dp)
-                end.linkTo(parent.end, margin = 8.dp)
-            }) {
-            Text("Delete")
+        var deleteButtonVisible: Boolean by remember { mutableStateOf(true) }
+        if (deleteButtonVisible) {
+            Button(onClick = postPageData.deleteButtonOnClick,
+                colors = defaultButtonColors(
+                    backgroundColor = Color.Red,
+                    contentColor = Color.White
+                ),
+                modifier = Modifier.constrainAs(deleteButtonRef) {
+                    top.linkTo(textFiledRef.bottom, margin = 16.dp)
+                    start.linkTo(buttonRef.end, margin = 8.dp)
+                    end.linkTo(parent.end, margin = 8.dp)
+                }) {
+                Text("Delete")
+            }
         }
     }
 }
